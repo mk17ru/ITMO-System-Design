341d6857265a6cf3f93fc2b509362fac3b165bd1
Nikita Stepanov <nikita.stepanov@assaia.com>
2024-10-13 01:52:12 +0400

    Save

--- src/commands/exit_command.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-from src.commands.base_command import BaseCommand
-
-
-class ExitCommand(BaseCommand):
-    def __call__(self) -> int:
-        sys.exit(0)
--- src/commands/pwd_command.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from src.commands.base_command import BaseCommand
-
-
-class PwdCommand(BaseCommand):
-    def __call__(self) -> int:
-        self.stdout.write(os.getcwd())
-        return 0
