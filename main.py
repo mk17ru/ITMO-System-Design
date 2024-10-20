6b38eaa5966c1212304b54d067efcb9e26047ffc
CHAAAPS <vitars2001@gmail.com>
2024-10-20 19:09:43 +0300

    change main

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from cli.commands import base_command
 
 def main_logic() -> int:
     """Implement main flow of CLI."""
+    parser_implementaion = parser.Parser()
     while True:
         try:
             raw_input_command: str = reader.Reader.read()
-            parsed_commands: list[base_command.BaseCommand] = parser.Parser.parse(raw_input_command)
+            parsed_commands: list[base_command.BaseCommand] = parser_implementaion.parse(raw_input_command)
             executor.Executor.execute(parsed_commands)
             presenter_ = presenter.Presenter()
             presenter_.show(executor.Executor.output_data)
