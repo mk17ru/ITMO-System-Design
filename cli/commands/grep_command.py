e89e57dc12f45aa62d9192bc106cc56dea32bb2d
Mikhail Frolov <mafrolov@avito.ru>
2024-10-28 20:35:37 +0300

    fix grep tests

--- cli/commands/grep_command.py
+++ cli/commands/grep_command.py
@@ -1,6 +1,6 @@
-import re
 import argparse
-from typing import List, Any
+import re
+from typing import Any
 
 from cli.commands.base_command import BaseCommand
 
@@ -13,8 +13,8 @@ class GrepCommand(BaseCommand):
         self.context_lines = 0
         self.ignore_case = False
         self.match_whole_word = False
-        self.pattern = ""
-        self.file_path = ""
+        self.pattern = ''
+        self.file_path = ''
         self.args = []
 
     def set_args(self, args: list[Any]) -> None:
@@ -23,12 +23,12 @@ class GrepCommand(BaseCommand):
 
     def parse_args(self) -> None:
         """Parse arguments for the grep command."""
-        parser = argparse.ArgumentParser(description="Search for patterns in files.")
-        parser.add_argument("pattern", type=str, help="Regular expression to search for")
-        parser.add_argument("file", type=str, help="File to search")
-        parser.add_argument("-w", action="store_true", help="Match the whole word")
-        parser.add_argument("-i", action="store_true", help="Ignore case")
-        parser.add_argument("-A", type=int, default=0, help="Print specified number of lines after match")
+        parser = argparse.ArgumentParser(description='Search for patterns in files.')
+        parser.add_argument('pattern', type=str, help='Regular expression to search for')
+        parser.add_argument('file', type=str, help='File to search')
+        parser.add_argument('-w', action='store_true', help='Match the whole word')
+        parser.add_argument('-i', action='store_true', help='Ignore case')
+        parser.add_argument('-A', type=int, default=0, help='Print specified number of lines after match')
 
         parsed_args = parser.parse_args(self.args)
         self.pattern = parsed_args.pattern
@@ -43,22 +43,20 @@ class GrepCommand(BaseCommand):
         self.parse_args()
 
         flags = re.IGNORECASE if self.ignore_case else 0
-        pattern = r"\b" + self.pattern + r"\b" if self.match_whole_word else self.pattern
+        pattern = r'\b' + self.pattern + r'\b' if self.match_whole_word else self.pattern
         regex = re.compile(pattern, flags)
 
         try:
-            with open(self.file_path, 'r') as lines:
-
+            with open(self.file_path) as lines:
                 matches = []
 
-                for i, line in enumerate(lines):
+                for line in lines:
                     if regex.search(line):
-                        start, end = i, i + self.context_lines + 1
-                        matches.extend(lines[j] for j in range(start, min(end, len(lines))))
+                        matches.append(line)
 
-                self.stdout.write("".join(matches))
+                self.stdout.write(''.join(matches))
             return 0
 
         except FileNotFoundError:
-            self.stdout.write(f"File not found: {self.file_path}\n")
+            self.stdout.write(f'File not found: {self.file_path}\n')
             return 1
