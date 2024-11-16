f892853e46a7f2d2a7f9a5777dc00efd2165a938
Mikhail Shadrin <shadrin.michael@yandex.ru>
2024-11-16 12:55:21 +0300

    Правки по коду от ruff format

--- tests/commands_tests/ls_test.py
+++ tests/commands_tests/ls_test.py
@@ -1,6 +1,6 @@
-import os
 from io import StringIO
 from unittest.mock import patch
+
 import pytest
 
 from cli.commands.ls_command import LsCommand
@@ -13,49 +13,43 @@ class TestLsCommand:
         command = LsCommand(stdout=mock_stdout)
         return command
 
-    @patch("os.listdir")
-    @patch("os.getcwd", return_value="/mocked/directory")
+    @patch('os.listdir')
+    @patch('os.getcwd', return_value='/mocked/directory')
     def test_ls_command_no_args(self, mock_getcwd, mock_listdir, ls_command):
-        mock_listdir.return_value = ["file1", "file2", "dir1"]
+        mock_listdir.return_value = ['file1', 'file2', 'dir1']
 
         ret_code = ls_command()
 
-        assert ls_command.stdout.getvalue() == "file1\nfile2\ndir1\n"
+        assert ls_command.stdout.getvalue() == 'file1\nfile2\ndir1\n'
         assert ret_code == 0
 
-    @patch("os.listdir")
+    @patch('os.listdir')
     def test_ls_command_with_args(self, mock_listdir, ls_command):
-        mock_listdir.return_value = ["file1", "file2", "dir1"]
-        test_path = "/custom/directory"
+        mock_listdir.return_value = ['file1', 'file2', 'dir1']
+        test_path = '/custom/directory'
         ls_command.set_args([test_path])
 
         ret_code = ls_command()
 
-        assert ls_command.stdout.getvalue() == "file1\nfile2\ndir1\n"
+        assert ls_command.stdout.getvalue() == 'file1\nfile2\ndir1\n'
         assert ret_code == 0
 
-    @patch("os.listdir", side_effect=FileNotFoundError)
+    @patch('os.listdir', side_effect=FileNotFoundError)
     def test_ls_command_directory_not_found(self, mock_listdir, ls_command):
-        test_path = "/non_existent/directory"
+        test_path = '/non_existent/directory'
         ls_command.set_args([test_path])
 
         ret_code = ls_command()
 
-        assert (
-            ls_command.stdout.getvalue()
-            == "Ошибка: Директория /non_existent/directory не найдена.\n"
-        )
+        assert ls_command.stdout.getvalue() == 'Ошибка: Директория /non_existent/directory не найдена.\n'
         assert ret_code == 1
 
-    @patch("os.listdir", side_effect=PermissionError)
+    @patch('os.listdir', side_effect=PermissionError)
     def test_ls_command_permission_denied(self, mock_listdir, ls_command):
-        test_path = "/restricted/directory"
+        test_path = '/restricted/directory'
         ls_command.set_args([test_path])
 
         ret_code = ls_command()
 
-        assert (
-            ls_command.stdout.getvalue()
-            == "Ошибка: Недостаточно прав для доступа к /restricted/directory.\n"
-        )
+        assert ls_command.stdout.getvalue() == 'Ошибка: Недостаточно прав для доступа к /restricted/directory.\n'
         assert ret_code == 1
